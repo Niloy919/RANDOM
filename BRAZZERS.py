db1c935cdbb5e71d39499fc90cae202c48476060
PSYCHO PICCHI <99023900+PSYCHO-PICCHI@users.noreply.github.com>
2022-11-05 18:40:02 +0600

    Update BRAZZERS.py

--- BRAZZERS.py
+++ BRAZZERS.py
@@ -3,13 +3,13 @@ import os,platform,time
 bitt=platform.architecture()[0]
  
 if bitt=="32bit":
-    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    print('Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import ERROR.main
  
  
 elif bitt=="64bit":
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    os.system('clear');print('Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import ERROR.main
  
